65b4c80030a5bd94711f977e35b9133777f086b0
Natacha <natacha.duval@actimage.com>
2011-01-27 17:22:34 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemobisApp::Application.config.secret_token = '8cdd78a3ac9146aa293c9bab8a0333703ec151b72edab9e9a98a284fc6b9b84c2219207a1235d7c0bbe5410cdd40a6143f1c2673521a3f0a0dbc747c1799bfc5'
